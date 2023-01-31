a22990c8961e624cb9f12451c84e8d375b2ba8ec
WorldFS <fsworld009@gmail.com>
2023-01-31 21:25:31 +0800

    add more options

--- batch_convert_movie_audio.sh
+++ batch_convert_movie_audio.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+set -e
+
 [ ! -d "$1" ] && echo "$1 isn't a valid path" && exit 1
 
 cd $1
-export bk_folder="wmvbk"
+export extension=${2:-"wmv"}
+export samplerate=${3:-"48000"}
+export bk_folder=${4:-"_original"}
 
 
-# revert backups if presented
+# revert backups
 if [ -d "$bk_folder" ]; then
     for file in $(ls $bk_folder); do
         mv "$bk_folder/$file" $file
@@ -16,13 +20,13 @@ fi
 mkdir -p $bk_folder
 
 for file in $(ls); do
-   if [[ $file =~ .*\.(wmv$) ]]; then
+   if [[ $file =~ .*\.($extension$) ]]; then
         # Move file to backup folder
         mv $file "$bk_folder/"
         # convert file audio and save to original location
         echo "Converting file $file..."
         # it is required to output to mkv otherwise ffmpeg refuses to write
-        ffmpeg -i $bk_folder/$file -vcodec copy -acodec libopus -b:a 48000 $file.mkv
+        ffmpeg -i $bk_folder/$file -vcodec copy -acodec libopus -b:a $samplerate $file.mkv
         mv $file.mkv $file
    fi
 done
